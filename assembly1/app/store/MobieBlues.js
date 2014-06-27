dc10c9bdd511153e5268fba89bc83d94c07988cd
Allan Brighton <Allan.Brighton@t-online.de>
2014-06-27 21:58:16 +0200

    Fixed JSON reading (still needs testing, work)

--- assembly1/app/store/MobieBlues.js
+++ assembly1/app/store/MobieBlues.js
@@ -4,11 +4,10 @@ Ext.define('Assembly1.store.MobieBlues', {
     model: 'Assembly1.model.MobieBlue',
     autoLoad: true,
     listeners: {
-        // TODO: put this in a controller?
         load: function (store, records, success, operations) {
             var form = Ext.getCmp('assembly1Form');
             // XXX Order might be different!?!
-            var m = Ext.create(Ext.ModelManager.getModel('Assembly1.model.MobieBlue'), {
+            var m = Ext.create('Assembly1.model.MobieBlue', {
                 filter: records[0].raw.setup['tmt.mobie.blue.filter'].value,
                 disperser: records[1].raw.setup['tmt.mobie.blue.disperser'].value
             });
