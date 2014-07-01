98161c25ab94236c060d69cc2c5d671cef5743fe
Allan Brighton <Allan.Brighton@t-online.de>
2014-07-01 16:43:14 +0200

    Made the order of  incoming setup configs unimportant

--- assembly1/app/store/MobieBlues.js
+++ assembly1/app/store/MobieBlues.js
@@ -3,13 +3,23 @@ Ext.define('Assembly1.store.MobieBlues', {
     extend:  'Ext.data.Store',
     model: 'Assembly1.model.MobieBlue',
     autoLoad: true,
+    getValue: function(records, prefix) {
+        for(rec in records) {
+            var setup = rec.raw.setup;
+            if (setup.hasOwnProperty(prefix)) {
+                return setup[prefix].value;
+            }
+        }
+        return null;
+    },
     listeners: {
         load: function (store, records, success, operations) {
             var form = Ext.getCmp('assembly1Form');
-            // XXX Order might be different!?!
+            var filter = getValue(records, 'tmt.mobie.blue.filter')
+            var disperser = getValue(records, 'tmt.mobie.blue.disperser')
             var m = Ext.create('Assembly1.model.MobieBlue', {
-                filter: records[0].raw.setup['tmt.mobie.blue.filter'].value,
-                disperser: records[1].raw.setup['tmt.mobie.blue.disperser'].value
+                filter: filter,
+                disperser: disperser
             });
             form.loadRecord(m);
         }
