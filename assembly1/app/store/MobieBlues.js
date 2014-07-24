8528a6861d8646c00cbd01336e6587ef442dad69
Allan Brighton <Allan.Brighton@t-online.de>
2014-07-25 00:00:52 +0200

    Fixed problem with for loop

--- assembly1/app/store/MobieBlues.js
+++ assembly1/app/store/MobieBlues.js
@@ -4,7 +4,8 @@ Ext.define('Assembly1.store.MobieBlues', {
     model: 'Assembly1.model.MobieBlue',
     autoLoad: true,
     getValue: function(records, prefix) {
-        for(rec in records) {
+        for (i = 0; i < records.length; i++) {
+            var rec = records[i];
             var setup = rec.raw.setup;
             if (setup.hasOwnProperty(prefix)) {
                 return setup[prefix].value;
@@ -15,8 +16,8 @@ Ext.define('Assembly1.store.MobieBlues', {
     listeners: {
         load: function (store, records, success, operations) {
             var form = Ext.getCmp('assembly1Form');
-            var filter = getValue(records, 'tmt.mobie.blue.filter')
-            var disperser = getValue(records, 'tmt.mobie.blue.disperser')
+            var filter = this.getValue(records, 'tmt.mobie.blue.filter');
+            var disperser = this.getValue(records, 'tmt.mobie.blue.disperser');
             var m = Ext.create('Assembly1.model.MobieBlue', {
                 filter: filter,
                 disperser: disperser
